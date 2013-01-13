aaf871bee2a19cacfa5d410240abe24f36fbdc47
daitomodachi <daitomodachi6@gmail.com>
2013-01-13 08:56:45 -0500

    Proper comments for main.c

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 //	Includes, defines, and global variables
 //===============================================================================================
 #include "controller.h"
+
+// config functions, do not mess with
 #pragma config FOSC = HS
 #pragma config WDT = OFF
 #pragma config PBADEN = OFF
@@ -21,7 +23,10 @@ char message[100];
 //===============================================================================================
 void main(void)
 {
+    // Creates the variable to readin the commands from the python script
     char reading;
+    
+    // Creates the controller structure and initizle the corresponding values
     CONTROLLER PY_GAMES_CONTROLLER;
     CONTROLLER_INIT(&PY_GAMES_CONTROLLER);
 
@@ -34,32 +39,36 @@ void main(void)
 
     while(1)
     {
+        //  Wait for the next branch of data to come in and store it to the reading variable
         while(!DataRdyUSART());
         reading=ReadUSART();
 
         // Checks to see if the current reading is equal to c, which means check to see if the controller is connected
         if(reading=='c')
         {
+            //  Sends 1 to the python script
             sprintf(message,"1");
             putsUSART(message);
             Delay1KTCYx(4);
         }
+        // Checks to see if reading is equal to j for joystick
         else if(reading=='j')
         {
-             // Waits for the next data to be sent
+             // Waits for the next data to be sent and stores it to reading
              while(!DataRdyUSART());
              reading=ReadUSART();
 
              // Checks to see if the next reading is equal to l, or left
              if(reading=='l')
              {
-                 // Waits for the next data to come in
+                 // Waits for the next data to come in and stores it to reading
                   while(!DataRdyUSART());
                   reading=ReadUSART();
 
                   //    Checks to see if x axis data is requested
                   if(reading=='x')
                   {
+                      // Reads in the left joystick's x axis and sends it to the python script
                       int result=READJOYSTICKX(&PY_GAMES_CONTROLLER.LEFT_JOYSTICK);
                       sprintf(message,"%d",result);
                       putsUSART(message);
@@ -68,6 +77,7 @@ void main(void)
                   //    Check to see if y axis data is requested
                   else if(reading=='y')
                   {
+                      // Reads the left joystick's y axis and sends the results to the python script
                       int result=READJOYSTICKY(&PY_GAMES_CONTROLLER.LEFT_JOYSTICK);
                       sprintf(message,"%d",result);
                       putsUSART(message);
@@ -76,15 +86,17 @@ void main(void)
 
 
              }
+             // Checks to see if reading is equal to r for right
              else if(reading=='r')
              {
-                 // Waits for the next data to come in
+                 // Waits for the next data to come in and stores the results to reading
                   while(!DataRdyUSART());
                   reading=ReadUSART();
 
                   //    Checks to see if x axis data is requested
                   if(reading=='x')
                   {
+                      // Reads in the right joystick's x axis and sends the results to the python script
                       int result=READJOYSTICKX(&PY_GAMES_CONTROLLER.RIGHT_JOYSTICK);
                       sprintf(message,"%d",result);
                       putsUSART(message);
@@ -93,6 +105,7 @@ void main(void)
                   //    Check to see if y axis data is requested
                   else if(reading=='y')
                   {
+                      // Checks the right joystick's y axis and sends the results to the python script
                       int result=READJOYSTICKY(&PY_GAMES_CONTROLLER.RIGHT_JOYSTICK);
                       sprintf(message,"%d",result);
                       putsUSART(message);
@@ -101,21 +114,27 @@ void main(void)
              }
 
         }
+        // Checks to see if reading is equal to b for button
         else if(reading=='b')
         {
-            // Waits for the next data to come in
+            // Waits for the next data to come in and stores the results to reading
             while(!DataRdyUSART());
             reading=ReadUSART();
-
+            
+            // Checks to see if reading is equal to y for yellow button
             if(reading=='y')
             {
+                // Checks the status of the yellow button and sends it to the python script
                 int result=ISBUTTONPRESSED(&PY_GAMES_CONTROLLER.YELLOW_BUTTON);
                 sprintf(message,"%d",result);
                 putsUSART(message);
                 Delay1KTCYx(4);
             }
+            
+            // Checks to see if reading is equal to r for red button
             else if(reading=='r')
             {
+                //  Checks the status of the red button and sends the results to the python script
                 int result=ISBUTTONPRESSED(&PY_GAMES_CONTROLLER.RED_BUTTON);
                 sprintf(message,"%d",result);
                 putsUSART(message);
